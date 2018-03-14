744a24cd58a8e69a717e3b7b3c223a89ef5d6da6
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 22:41:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,67 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def reverse_array(array)
+  array = array.reverse
+end
+
+def kesha_maker(array)
+  i = 0
+  new_array = []
+  array.each do |item|
+    item[2] = "$"
+    new_array << item
+    i += 1
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |item|
+    item[0] == 'a'
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |item|
+    sum += item
+  end
+  sum
+end
+
+def add_s(array)
+  new_array = []
+  i = 0
+  array.each do |item|
+    if i != 1
+      new_array << "#{item}s"
+    else
+      new_array << item
+    end
+    i += 1
+  end
+  new_array
+end
